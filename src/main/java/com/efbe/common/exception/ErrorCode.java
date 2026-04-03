02e2cc59f82bbfa64a4f6dbd20290b9ce1b1a3e4
Hxxjeong <heejeong3394@gmail.com>
2026-04-04 01:05:00 +0900

    [Feat]: 응답, 예외 템플릿 작성

---
+++ src/main/java/com/efbe/common/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.efbe.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    ;
+
+    private final int code;
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
