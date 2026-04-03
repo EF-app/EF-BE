922c423e5495e0a8ead5b51d3261df2bfc8fe820
Hxxjeong <heejeong3394@gmail.com>
2026-04-04 00:51:47 +0900

    first commit

---
+++ src/main/java/com/efbe/EfBeApplication.java
@@ -0,0 +1,13 @@
+package com.efbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfBeApplication.class, args);
+    }
+
+}
